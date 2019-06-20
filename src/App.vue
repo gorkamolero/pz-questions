9277ab18260139853c73a4fb7ae7fc3dc86205e5
Gorka <gorka.molero@gmail.com>
2019-06-20 14:04:51 +0200

    extracting comps

--- src/App.vue
+++ src/App.vue
@@ -19,15 +19,4 @@ export default {
   height: 100%;
   border: var(--space-l) solid whitesmoke;
 }
-.Container {
-  width: calc(100vw / 3);
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  & + & { border-left: 1px solid }
-}
-
-.puiSpaceOut { >  * + * { margin-left: var(--space-s) } }
 </style>
