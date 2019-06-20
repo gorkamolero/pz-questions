27b83e2f4bb4c5efb704d9c19322e54b9da021aa
Gorka <gorka.molero@gmail.com>
2019-06-20 14:05:01 +0200

    extracting comps

---
+++ src/components/core/Typer.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="typer" :class="{'fullcenter': fullcenter}" >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['fullcenter']
+}
+</script>
+
+<style lang="scss">
+  .typer {
+    max-width: 320px;
+    margin: 0 auto;
+    font-size: 90%;
+    line-height: 1.618;
+
+    h1 {
+      font-size: 2em;
+      margin-bottom: .618em;
+    }
+
+    h3 { font-size: 1.1em; }
+
+    a, a:visited { color: blue }
+    pre { margin: 1em 0; }
+    code {
+      font-size: 80%;
+      background: rgba(0, 0, 0, .5);
+      color: white;
+      padding: 3px;
+    }
+    * + * { margin-top: .618em; }
+
+    &.fullcenter {
+      height: 100vh;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+</style>
---
+++ src/components/core/styles/Styles.vue
@@ -0,0 +1,26 @@
+<template><span class="styles-loaded"></span></template>
+
+<style lang="scss">
+ // Welcome to the Pickzen SCSS party!
+  @import '~normalize-scss/sass/normalize';
+  @include normalize();
+  
+  @import '~vue2-animate/src/sass/vue2-animate';
+  @keyframes fadeInUp {
+    from {
+      opacity: 0;
+      transform: translate3d(0, 10%, 0);
+    }
+
+    to {
+      opacity: 1;
+      transform: none;
+    }
+  }
+
+  @import './vars', './core', './helpers', './reset';
+
+  // Go ahead, children
+  
+
+</style>
